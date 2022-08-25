cc81e2a84d38c34f3f6f19b913e301de68c639a4
Alfredo Bonell <99234057+AlfredoBonell@users.noreply.github.com>
2022-08-24 20:23:52 -0500

    API Contalink
    - Api hecha en Java.

---
+++ contalink/contalink/src/main/java/backend/contalink/ContalinkApplication.java
@@ -0,0 +1,13 @@
+package backend.contalink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContalinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContalinkApplication.class, args);
+	}
+
+}
